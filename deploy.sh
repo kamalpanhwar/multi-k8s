cfcce1a55e8dd40feff07ae23673e0e0fc159c8f
Kamal uddin Panhwar <kamalpanhwar@gmail.com>
2024-05-29 22:09:32 +0100

    added configurations for letsencrypt

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@ docker build -t kamalpanhwar/multi-client:latest -t kamalpanhwar/multi-client:$S
 docker build -t kamalpanhwar/multi-server:latest -t kamalpanhwar/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t kamalpanhwar/multi-worker:latest -t kamalpanhwar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push kamalpanhwar/multi-client:latest
-docker push kamalpanhwar/multi-client:$SHA
 docker push kamalpanhwar/multi-server:latest
-docker push kamalpanhwar/multi-server:$SHA
 docker push kamalpanhwar/multi-worker:latest
+docker push kamalpanhwar/multi-client:$SHA
+docker push kamalpanhwar/multi-server:$SHA
 docker push kamalpanhwar/multi-worker:$SHA
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=kamalpanhwar/multi-server:$SHA
-kubectl set image deployments/client-deployment server=kamalpanhwar/multi-client:$SHA
-kubectl set image deployments/worker-deployment server=kamalpanhwar/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=kamalpanhwar/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=kamalpanhwar/multi-worker:$SHA
